9cbd27bee8d64a4d845b8179cf85fdbe9490c826
guidopermanEleva <guido.perman@leniolabs.com>
2024-03-30 23:17:29 -0300

    header markup added

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, inject } from '@angular/core';
+import { MealService } from '../../services/meal.service';
+import { Observable } from 'rxjs';
+import { Category } from '../../interfaces/categories.interface';
 
 @Component({
   selector: 'app-header',
@@ -9,4 +12,12 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './header.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  mealService = inject(MealService)
+  categories$: Observable<Category[]> = this.mealService.getCategories()
+
+  ngOnInit(): void {
+      this.categories$.subscribe(data=>console.log(data))
+  }
+
+}
