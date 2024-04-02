ce0a0595a201ec84096b89191f332f40e8dc6671
guidopermanEleva <guido.perman@leniolabs.com>
2024-04-02 09:03:47 -0300

    home component, meal card y loading

--- src/app/components/meal-card/meal-card.component.ts
+++ src/app/components/meal-card/meal-card.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Meal } from '../../interfaces/meal.interface';
+import { LoadingComponent } from '../loading/loading.component';
 
 @Component({
   selector: 'app-meal-card',
   standalone: true,
-  imports: [],
+  imports: [LoadingComponent],
   templateUrl: './meal-card.component.html',
   styleUrl: './meal-card.component.scss'
 })
 export class MealCardComponent {
+  @Input() meal!: Meal;
 
 }
